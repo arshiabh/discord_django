765c51ae72aa3e97c5e5f40b7763ff45e5e68c29
arshia <arshiabohloli@gmail.com>
2024-10-27 17:16:10 +0330

    database changes

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name
+    
+
+class Massage(models.Model):
+    # user = User
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.body[0:50]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render
+from .models import Room
 
-rooms = [
-    {'id':1, "name":"frontend"},
-    {'id':2, "name":"learn back"},
-    {'id':3, "name":"learn angular"}
-]
+
+# rooms = [
+#     {'id':1, "name":"frontend"},
+#     {'id':2, "name":"learn back"},
+#     {'id':3, "name":"learn angular"}
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     return render(request, 'api/home.html',{"rooms":rooms})
 
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-            
+    room = Room.objects.get(id=pk)            
     context = {
         'room':room
     }
